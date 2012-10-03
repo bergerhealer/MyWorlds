31b13f22a413ba64e84c18623bb97d1c0a85ea65
Irmo van den Berge <bergerkiller@gmail.com>
2012-10-03 18:52:38 +0200

    Changed keep inventory default to false and added togglerespawn msgs'

--- src/com/bergerkiller/bukkit/mw/commands/WorldToggleRespawn.java
+++ src/com/bergerkiller/bukkit/mw/commands/WorldToggleRespawn.java
@@ -19,8 +19,10 @@ public class WorldToggleRespawn extends Command {
 			wc.forcedRespawn = !wc.forcedRespawn;
 			if (wc.forcedRespawn) {
 				message(ChatColor.GREEN + "Forced respawning to world spawn on World: '" + worldname + "' enabled!");
+				message(ChatColor.GREEN + "People will return to the world spawn instead of their custom (bed) spawn.");
 			} else {
 				message(ChatColor.YELLOW + "Forced respawning to world spawn on World: '" + worldname + "' disabled!");
+				message(ChatColor.GREEN + "People will return to their custom (bed) spawn instead of the world spawn.");
 			}
 		}
 	}
