6631c9621ab6b7c22c5d2edd9e0ca002163c233b
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2024-10-06 19:55:42 +0200

    Add more debuggable events, clean it up
    Signed-off-by: Irmo van den Berge <irmo.vandenberge@ziggo.nl>

--- src/main/java/com/bergerkiller/bukkit/mw/commands/WorldDebug.java
+++ src/main/java/com/bergerkiller/bukkit/mw/commands/WorldDebug.java
@@ -5,10 +5,16 @@ import com.bergerkiller.bukkit.mw.Permission;
 import com.bergerkiller.bukkit.mw.utils.EventListenerHook;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
+import org.bukkit.event.entity.EntityTeleportEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.world.PortalCreateEvent;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -16,9 +22,62 @@ import java.util.List;
 import java.util.Map;
 
 public class WorldDebug extends Command {
-    private static final Map<String, Class<? extends Event>> eventNames = new HashMap<>();
+    private static final Map<String, EventDebugger<?>> eventNames = new HashMap<>();
+
     static {
-        eventNames.put("portal_create", PortalCreateEvent.class);
+        registerDetectCancel("portal_create", PortalCreateEvent.class, (listener, callEvent, event) -> {
+            Entity entity = event.getEntity();
+            String entityName = "[No Entity]";
+            if (entity instanceof Player) {
+                entityName = "Player " + ((Player) entity).getName();
+            } else if (entity != null) {
+                entityName = "Entity [" + entity.getType().name() + "]";
+            }
+            Bukkit.broadcastMessage("[MyWorlds] " + event.getClass().getSimpleName() +
+                    " by " + entityName + " reason " + event.getReason().name() +
+                    " was cancelled by plugin " + listener.getPlugin().getName());
+        });
+        registerDetectCancel("player_teleport", PlayerTeleportEvent.class, (listener, callEvent, event) -> {
+            Location to = event.getTo();
+            String toString = "[none]";
+            if (to != null) {
+                toString = "[x=" + to.getBlockX() + ", y=" + to.getBlockY() + ", z=" + to.getBlockZ() + "]";
+            }
+            Bukkit.broadcastMessage("[MyWorlds] " + event.getClass().getSimpleName() +
+                    " of " + event.getPlayer().getName() + " to " + toString +
+                    " was cancelled by plugin " + listener.getPlugin().getName());
+        });
+        registerDetectCancel("entity_teleport", EntityTeleportEvent.class, (listener, callEvent, event) -> {
+            Entity entity = event.getEntity();
+            String entityName = "[No Entity]";
+            if (entity instanceof Player) {
+                entityName = "Player " + ((Player) entity).getName();
+            } else if (entity != null) {
+                entityName = "Entity [" + entity.getType().name() + "]";
+            }
+            Location to = event.getTo();
+            String toString = "[none]";
+            if (to != null) {
+                toString = "[x=" + to.getBlockX() + ", y=" + to.getBlockY() + ", z=" + to.getBlockZ() + "]";
+            }
+            Bukkit.broadcastMessage("[MyWorlds] " + event.getClass().getSimpleName() +
+                    " of " + entityName + " to " + toString +
+                    " was cancelled by plugin " + listener.getPlugin().getName());
+        });
+    }
+
+    private static <T extends Event & Cancellable> void registerDetectCancel(String name, Class<T> eventClass, EventListenerHook.Handler<T> handler) {
+        register(name, eventClass, (listener, callEvent, event) -> {
+            boolean wasCancelled = event.isCancelled();
+            callEvent.accept(event);
+            if (!wasCancelled && event.isCancelled()) {
+                handler.handle(listener, callEvent, event);
+            }
+        });
+    }
+
+    private static <T extends Event> void register(String name, Class<T> eventClass, EventListenerHook.Handler<T> handler) {
+        eventNames.put(name, new EventDebugger<>(eventClass, handler));
     }
 
     public WorldDebug() {
@@ -30,52 +89,61 @@ public class WorldDebug extends Command {
         if (args.length == 0) {
             sender.sendMessage("/mw debug [eventname] - Debug events");
         } else {
-            Class<? extends Event> eventClass = eventNames.get(args[0]);
-            if (eventClass == null) {
+            EventDebugger<?> debugger = eventNames.get(args[0]);
+            if (debugger == null) {
                 sender.sendMessage(ChatColor.RED + "Unknown event: " + args[0]);
             } else {
-                executeDebugEvent(args[0], eventClass);
-            }
-        }
-    }
-
-    private void executeDebugEvent(String eventName, Class<? extends Event> eventClass) {
-        removeArg(0);
-        boolean enabled;
-        if (args.length == 0) {
-            enabled = true;
-        } else {
-            enabled = ParseUtil.parseBool(args[0], false);
-        }
-        sender.sendMessage(ChatColor.RED + eventClass.getSimpleName() + " debug mode: " + enabled);
-        if (enabled) {
-            EventListenerHook.hook(eventClass, (listener, callEvent, event) -> {
-                Cancellable cancellable = (Cancellable) event;
-                boolean wasCancelled = cancellable.isCancelled();
-                callEvent.accept(event);
-                if (
-                        !wasCancelled && cancellable.isCancelled()
-                ) {
-                    Bukkit.broadcastMessage("[MyWorlds] " + eventClass.getSimpleName() +
-                            " was cancelled by plugin " + listener.getPlugin().getName());
+                String eventName = args[0];
+                removeArg(0);
+                boolean enabled;
+                if (args.length == 0) {
+                    enabled = true;
+                } else {
+                    enabled = ParseUtil.parseBool(args[0], false);
                 }
-            });
-            sender.sendMessage(ChatColor.YELLOW + "A message will be broadcast when a " + eventClass.getSimpleName() +
-                    " is cancelled by a plugin");
-            sender.sendMessage(ChatColor.YELLOW + "Use /mw debug event " + eventName + " false to turn off again");
-        } else {
-            EventListenerHook.unhook(eventClass);
+                if (enabled) {
+                    sender.sendMessage(ChatColor.YELLOW + "Debugging event " + eventName + ": " + ChatColor.GREEN + "Enabled");
+                } else {
+                    sender.sendMessage(ChatColor.YELLOW + "Debugging event " + eventName + ": " + ChatColor.RED + "Disabled");
+                }
+                if (enabled) {
+                    debugger.hook();
+                    sender.sendMessage(ChatColor.YELLOW + "A message will be broadcast when a " + debugger.eventClass.getSimpleName() +
+                            " is cancelled by a plugin");
+                    sender.sendMessage(ChatColor.YELLOW + "Use /mw debug event " + eventName + " false to turn off again");
+                } else {
+                    debugger.unhook();
+                }
+            }
         }
     }
 
     @Override
     public List<String> autocomplete() {
         if (args.length == 1) {
-            return Arrays.asList("portal_create");
+            return new ArrayList<>(eventNames.keySet());
         } else if (args.length == 2) {
             return Arrays.asList("yes", "no");
         } else {
             return Collections.emptyList();
+        }
+    }
+
+    private static class EventDebugger<T extends Event> {
+        public final Class<T> eventClass;
+        public EventListenerHook.Handler<T> handler;
+
+        public EventDebugger(Class<T> eventClass,  EventListenerHook.Handler<T> handler) {
+            this.eventClass = eventClass;
+            this.handler = handler;
+        }
+
+        public void hook() {
+            EventListenerHook.hook(eventClass, handler);
+        }
+
+        public void unhook() {
+            EventListenerHook.unhook(eventClass);
         }
     }
 }
